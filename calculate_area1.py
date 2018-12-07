34e79102755a706c5887c96719dd214390298d7f
David <davhunt@indiana.edu>
2018-12-07 14:36:14 -0500

    dir

--- calculate_area1.py
+++ calculate_area1.py
@@ -8,6 +8,7 @@ tmpdir='/tmp/' + str(sys.argv[1])
 fwhm=sys.argv[4]
 #subjects_dir = os.path.dirname(os.path.realpath(sys.argv[3]))
 subjects_dir = os.environ['SUBJECTS_DIR']
+subjects_dir = os.path.dirname(os.path.realpath(subjects_dir))
 subject_id=os.path.basename(os.path.normpath(sys.argv[3]))
 n_surfs=int(sys.argv[2])
 
@@ -17,5 +18,5 @@ for hemisphere in ("rh", "lh"):
 			areaf = "white"
 		elif areafile == ".area.pial":
 			areaf = "pial"
-		subprocess.call("mris_fwhm --s " + subject_id + " --hemi " + hemisphere + " --cortex --smooth-only --fwhm " + str(fwhm) + " --i " + os.path.join(subjects_dir,"surf", hemisphere+areafile) + " --o " + os.path.join(tmpdir,"%s_%s_area.mgh" %(hemisphere,areaf)), shell=True)
-		subprocess.call("cp " + os.path.join(subjects_dir,"surf",hemisphere+"."+areaf) + " " + tmpdir, shell=True)
+		subprocess.call("mris_fwhm --s " + subject_id + " --hemi " + hemisphere + " --cortex --smooth-only --fwhm " + str(fwhm) + " --i " + os.path.join(subjects_dir,subject_id,"surf", hemisphere+areafile) + " --o " + os.path.join(tmpdir,"%s_%s_area.mgh" %(hemisphere,areaf)), shell=True)
+		subprocess.call("cp " + os.path.join(subjects_dir,subject_id,"surf",hemisphere+"."+areaf) + " " + tmpdir, shell=True)
